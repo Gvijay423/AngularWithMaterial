9b6f196967e82888f7438b083d64ed18396cf108
RajuKottedi <raju.kottedi@gmail.com>
2017-06-11 13:31:56 +0300

    added support to run in vscode

--- src/AngularWebpackVisualStudio/Program.cs
+++ src/AngularWebpackVisualStudio/Program.cs
@@ -17,6 +17,7 @@ namespace Angular2WebpackVisualStudio
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001/")
                 .Build();
 
             host.Run();
