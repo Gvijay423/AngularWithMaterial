e4900d24074568ddbaa63233505b8e4a3898fc5d
RajuKottedi <raju.kottedi@gmail.com>
2017-06-20 11:54:32 +0300

    #Custom Pipe with multiple parameters

--- src/AngularWebpackVisualStudio/angularApp/app/customPipe/default.pipe.ts
+++ src/AngularWebpackVisualStudio/angularApp/app/customPipe/default.pipe.ts
@@ -5,12 +5,15 @@ import {Pipe} from '@angular/core';
 })
 
 export class DefaultPipe {
-    transform(value: string, fallback: string): string {
+    transform(value: string, fallback: string, append: boolean): string {
         let str = "";
         if (value) {
             str = value;
         } else {
             str = fallback;
+        }
+        if (append) {
+            str += " Appended String";
         }
         return str;
     }
